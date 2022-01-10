8f37aaf328b2d9f2f47c8d45ec59966863292c9c
kronosGR <geo.elgeo@gmail.com>
2022-01-10 13:31:38 +0100

    added button functionality

--- script.js
+++ script.js
@@ -1,10 +1,29 @@
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+
 let apiQuotes = [];
 
 // show new quote
 function newQuote() {
   // pick a random quote
   const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
-  console.log(quote);
+
+  if (!quote.author) {
+    authorText.textContent = none;
+  } else {
+    authorText.textContent = quote.author;
+  }
+
+  if (quote.text.length > 80) {
+    quoteText.classList.add('long-quote');
+  } else {
+    quoteText.classList.remove('long-quote');
+  }
+
+  quoteText.textContent = quote.text;
 }
 
 // Get quotes from API
@@ -19,5 +38,13 @@ async function getQuotes() {
     // Error handling
   }
 }
+
+function tweetQuote() {
+  const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+  window.open(twitterUrl, '_blank');
+}
+
+newQuoteBtn.addEventListener('click', newQuote);
+twitterBtn.addEventListener('click', tweetQuote);
 
 getQuotes();
