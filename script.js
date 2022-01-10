afd0ba93b0d82e67a19b2010aab3a079923fa073
kronosGR <geo.elgeo@gmail.com>
2022-01-10 13:22:11 +0100

    get the quotes with fetch

---
+++ script.js
@@ -0,0 +1,23 @@
+let apiQuotes = [];
+
+// show new quote
+function newQuote() {
+  // pick a random quote
+  const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+  console.log(quote);
+}
+
+// Get quotes from API
+async function getQuotes() {
+  const apiUrl = 'https://type.fit/api/quotes';
+
+  try {
+    const response = await fetch(apiUrl);
+    apiQuotes = await response.json();
+    newQuote();
+  } catch (error) {
+    // Error handling
+  }
+}
+
+getQuotes();
